a1a02d06121a8994953db3d282125ac427acc67a
Amanda Rolon <abrolon87@gmail.com>
2019-12-25 05:07:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,39 +9,39 @@ class Shoe
   # def brand=(brand)
   #   @brand = brand
   # end
-  # 
+  #
   # def brand
   #   @brand
   # end
-  # 
+  #
   # def color=(color)
   #   @color = color
   # end
-  # 
+  #
   # def color
   #   @color
   # end
-  # 
+  #
   # def size=(size)
   #   @size = size
   # end
-  # 
+  #
   # def size
   #   @size
   # end
-  # 
+  #
   # def material=(material)
   #   @material = material
   # end
-  # 
+  #
   # def material
   #   @material
   # end
-  # 
+  #
   # def condition=(condition)
   #   @condition = condition
   # end
-  # 
+  #
   # def condition
   #   @condition
   # end
