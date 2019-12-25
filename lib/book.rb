6282d8c6372f2a1231bbad375c4ae84906c7e3f1
Amanda Rolon <abrolon87@gmail.com>
2019-12-25 05:03:49 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,19 +1,19 @@
 class Book
 
   attr_accessor :author, :page_count, :genre
-
+  attr_reader :title
   def initialize(title)
     @title = title
   end
 
 
-  def title=(title)
-    @title = title
-  end
-
-  def title
-    @title
-  end
+  # def title=(title)
+  #   @title = title
+  # end
+  #
+  # def title
+  #   @title
+  # end
   #
   # def author=(author)
   #
